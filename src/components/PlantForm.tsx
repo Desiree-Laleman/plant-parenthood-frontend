0358e63624477457f8c360797b6515eead70a402
Alexa Joerin <alexa.joerin@gmail.com>
2023-09-14 08:58:29 -0600

    worked on components

--- src/components/PlantForm.tsx
+++ src/components/PlantForm.tsx
@@ -1,10 +1,34 @@
+import { FormEvent, useState } from "react";
 import "./PlantForm.css";
 
 const PlantForm = () => {
+  const [nickname, setNickname] = useState("");
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (e: FormEvent): void => {
+    e.preventDefault();
+    setNickname("");
+    setSearch("");
+  };
   return (
-    <form className="PlantForm">
-      <label htmlFor="nickname">Plant Nickname:</label>
-      <input type="text" name="nickname" id="nickname" />
+    <form className="PlantForm" onSubmit={(e) => handleSubmit(e)}>
+      <input
+        type="text"
+        name="nickname"
+        id="nickname"
+        placeholder="Plant Nickname"
+        value={nickname}
+        onChange={(e) => setNickname(e.target.value)}
+      />
+      <input
+        type="text"
+        name="search"
+        id="search"
+        placeholder="Plant Search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button>Add Plant</button>
     </form>
   );
 };
