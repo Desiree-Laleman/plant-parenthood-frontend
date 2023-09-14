5025e40d170ddbe7743e04f53383a2dc723251c1
Byron Lee Jr <leebyron120@gmail.com>
2023-09-14 16:54:41 -0400

    plants pop up after search

--- src/components/SearchResultsList.tsx
+++ src/components/SearchResultsList.tsx
@@ -1,18 +1,28 @@
 import { useState } from "react";
 import SearchResultsCard from "./SearchResultsCard";
 import "./SearchResultsList.css";
+import PerenualPlant from "../models/PerenualPlant";
+import Plant from "../models/Plant";
 
 interface Props {
   setShowNumber: (number: number) => void;
+  searchResults: PerenualPlant[];
+  setChosenPlant: (plant: Plant) => void;
 }
 
-const SearchResultsList = ({ setShowNumber }: Props) => {
-  const [searchResults, setSearchResults] = useState([]);
-
+const SearchResultsList = ({
+  setShowNumber,
+  searchResults,
+  setChosenPlant,
+}: Props) => {
   return (
     <ul className="SearchResultsList">
       {searchResults.map((item) => (
-        <SearchResultsCard item={item} setShowNumber={setShowNumber} />
+        <SearchResultsCard
+          item={item}
+          setShowNumber={setShowNumber}
+          setChosenPlant={setChosenPlant}
+        />
       ))}
     </ul>
   );
