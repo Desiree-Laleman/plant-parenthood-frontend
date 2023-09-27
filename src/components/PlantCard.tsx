3884edbabed6e364e27ec36039dc9dc752ae83ee
Desiree <dezlaleman@gmail.com>
2023-09-27 10:39:38 -0400

    brain fried

--- src/components/PlantCard.tsx
+++ src/components/PlantCard.tsx
@@ -1,5 +1,7 @@
+import Calendar from "react-calendar";
 import Plant from "../models/Plant";
 import "./PlantCard.css";
+import { useState } from "react";
 
 interface Props {
   plant: Plant;
@@ -20,6 +22,8 @@ const PlantCard = ({
   searchedPlantById,
   calculateTimeRemaining,
 }: Props) => {
+  // const [showCalendar, setShowCalendar] = useState(false);
+
   const handleClick = () => {
     setEditIndex();
     setShowNumber(0);
@@ -44,20 +48,28 @@ const PlantCard = ({
         onClick={() => seeDetails()}
       />
       <p>{plant.nickName}</p>
-      {calculateTimeRemaining(plant) < -1 ? (
-        <button className="overdue" onClick={waterButtonHandler}>
-          Overdue <i className="fa-solid fa-triangle-exclamation"></i>
+      <div id="watered-date-container">
+        {calculateTimeRemaining(plant) < -1 ? (
+          <button className="overdue" onClick={waterButtonHandler}>
+            Overdue <i className="fa-solid fa-triangle-exclamation"></i>
+          </button>
+        ) : calculateTimeRemaining(plant) > -1 &&
+          calculateTimeRemaining(plant) < 0 ? (
+          <button className="waterMe" onClick={waterButtonHandler}>
+            Water Me!
+          </button>
+        ) : (
+          <button className="watered" onClick={waterButtonHandler}>
+            Water in {Math.ceil(calculateTimeRemaining(plant))} days
+          </button>
+        )}
+        <button
+          id="calendar"
+          // onClick={() => setShowCalendar(true), <Calendar onChange={date} value={date} />}
+        >
+          <i className="fa-regular fa-calendar"></i>
         </button>
-      ) : calculateTimeRemaining(plant) > -1 &&
-        calculateTimeRemaining(plant) < 0 ? (
-        <button className="waterMe" onClick={waterButtonHandler}>
-          Water Me!
-        </button>
-      ) : (
-        <button className="watered" onClick={waterButtonHandler}>
-          Water in {Math.ceil(calculateTimeRemaining(plant))} days
-        </button>
-      )}
+      </div>
       <button id="edit-button" onClick={handleClick}>
         Edit
       </button>
