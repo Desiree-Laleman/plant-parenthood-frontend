a0261d8d83e771be0a34a09f23bd824f4b21f758
Desiree <dezlaleman@gmail.com>
2023-09-27 17:21:16 -0400

    calendar functionality

--- src/components/PlantCard.tsx
+++ src/components/PlantCard.tsx
@@ -1,7 +1,7 @@
-import Calendar from "react-calendar";
+// import Calendar from "react-calendar";
 import Plant from "../models/Plant";
 import "./PlantCard.css";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import "react-calendar/dist/Calendar.css";
 
 interface Props {
@@ -14,10 +14,6 @@ interface Props {
   calculateTimeRemaining: (plant: Plant) => number;
 }
 
-type ValuePiece = Date | null;
-
-type Value = ValuePiece | [ValuePiece, ValuePiece];
-
 const PlantCard = ({
   plant,
   deletePlantHandler,
@@ -27,23 +23,30 @@ const PlantCard = ({
   searchedPlantById,
   calculateTimeRemaining,
 }: Props) => {
-  const [date, setDate] = useState<Value>(new Date());
-  const [showCalendar, setShowCalendar] = useState(false);
+  const [date, setDate] = useState("");
+  // const [showCalendar, setShowCalendar] = useState(false);
 
   const handleClick = () => {
     setEditIndex();
     setShowNumber(0);
   };
 
-  const calClickHandler = () => {
-    setShowCalendar((prev) => !prev);
-  };
-
-  const waterButtonHandler = () => {
+  const handleSubmit = (e: FormEvent): void => {
+    e.preventDefault();
     const plantCopy = { ...plant };
-    plantCopy.waterDate = new Date();
+    plantCopy.waterDate = new Date(date);
     editPlantHandler(plantCopy);
   };
+
+  // const calClickHandler = () => {
+  //   setShowCalendar((prev) => !prev);
+  // };
+
+  // const waterButtonHandler = () => {
+  //   const plantCopy = { ...plant };
+  //   plantCopy.waterDate = new Date();
+  //   editPlantHandler(plantCopy);
+  // };
 
   const seeDetails = async () => {
     await searchedPlantById(plant.perenualId);
@@ -60,7 +63,7 @@ const PlantCard = ({
         onClick={() => seeDetails()}
       />
       <p>{plant.nickName}</p>
-      <div id="watered-date-container">
+      {/* <div id="watered-date-container">
         {calculateTimeRemaining(plant) < -1 ? (
           <button className="overdue" onClick={waterButtonHandler}>
             Overdue <i className="fa-solid fa-triangle-exclamation"></i>
@@ -79,7 +82,38 @@ const PlantCard = ({
           <i className="fa-regular fa-calendar"></i>
         </button>
       </div>
-      {showCalendar && <Calendar onChange={setDate} value={date} />}
+      {showCalendar && <Calendar onChange={setDate} value={date} />} */}
+      <div>
+        {/* <p>Water in {Math.ceil(calculateTimeRemaining(plant))} days</p> */}
+        {calculateTimeRemaining(plant) < -1 ? (
+          <p className="overdue">
+            Overdue <i className="fa-solid fa-triangle-exclamation"></i>
+          </p>
+        ) : calculateTimeRemaining(plant) > -1 &&
+          calculateTimeRemaining(plant) < 0 ? (
+          <p className="waterMe">I Need Water!</p>
+        ) : (
+          <p className="watered">
+            Water in {Math.ceil(calculateTimeRemaining(plant))} days
+          </p>
+        )}
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <label htmlFor="date">When did you water last?</label>
+          <div id="date-input">
+            <input
+              type="date"
+              defaultValue={date.toString()}
+              value={date}
+              onChange={(e) => {
+                setDate(e.target.value);
+              }}
+            />
+            <button>
+              <i className="fa-solid fa-check"></i>
+            </button>
+          </div>
+        </form>
+      </div>
       <button id="edit-button" onClick={handleClick}>
         Edit
       </button>
